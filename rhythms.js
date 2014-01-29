856145df6c99ed7feb804d9b27ac8fdab9201cc1
Ryan Whitley <rwhitley@spatialdev.com>
2014-01-29 11:44:53 -0800

    main changes

---
+++ rhythms.js
@@ -0,0 +1,64 @@
+//This is the main rhythms file.
+//Add or remove rhythms here and the main list of rhythms will be updated.
+
+var rhythms = {
+
+//Each rhythm name is enclosed in quotes
+
+"Balakulanyan": { audio : [], pcfiles : ["Bala Kuladyan.pcc"] },
+
+"Bando Djeli": { audio : [], pcfiles : ["Bando Djeli root and echauff.pcc"] },
+
+"Bolo Konondo": { audio : [], pcfiles : ["Bolokonondo.pcc"], videos : ['<iframe width="560" height="315" src="http://www.youtube.com/embed/VHRsj5EzcQ4?list=PLC5AAFA21E68EED43" frameborder="0" allowfullscreen></iframe>'] },
+
+"Demosoni Kelen": { audio : [], pcfiles : ["Demosoni Kelen w echauffe.pcc", "Demosoni Kelen.pcc"] },
+
+"Dibon": { audio : ["Billy Konate - Dibon.mp3", "Wulabakan - Dibon.mp3"], pcfiles : ["Dibon 2.pcc", "Dibon 3.pcc"] },
+
+"Dunun Kura": { audio : [], pcfiles : ["Dunun Kura [Nyewa Kodo].pcc"] },
+
+"Dunungbe": { audio : [], pcfiles : ["Dunungbe (1).pcc", "dunungbe echauff only.pcc", "Dunungbe variation.pcc", "Dunungbe w echauff.pcc", "Dunungbe.pcc"] },
+
+"Fakoli": { audio : [], pcfiles : ["Fakoli - Wadaba.pcc"] },
+
+"Ferabanka": { audio : [], pcfiles : ["ferabanka.pcc"] },
+
+"Gbada": { audio : [], pcfiles : ["Gbada_skeleton_w_Wadaba_dununba_part.pcc"] },
+
+"Gbereduka": { audio : [], pcfiles : ["Gbereduka_root.pcc"] },
+
+"Ikankorota": { audio : [], pcfiles : ["Ikankorota.pcc"] },
+
+"Kassa gbe (Simp)": { audio : ["Famoudou Konate - Kassa medley.mp3"], pcfiles : ["Kassa Simp w S_D var.pcc"] },
+
+"Kassa Ni Soro": { audio : ["Antioquia - Kassa Ni Soro.mp3", "Noumoudy Keita - Kassalissoro.mp3"], pcfiles : ["Kassa ni Soro root.pcc"] },
+
+"Konkoba": { audio : [], pcfiles : ["konkoba.pcc"] },
+
+"Konmasi": { audio : [], pcfiles : ["Konmasi.pcc"] },
+
+"Konowoulen": { audio : [], pcfiles : ["Konowoulen 1 w echauffe-1.pcc"] },
+
+"Lolo": { audio : [], pcfiles : ["lolo w break.pcc"] },
+
+"Maraka": { audio : [], pcfiles : ["maraka.pcc"] },
+
+"Moribayassa": { audio : [], pcfiles : ["moribayassa w mamady break.pcc"] },
+
+"N'fakaba": { audio : ["Diarra Konate - N'Fa Kaba.mp3", "Famoudou - K n wule II Nfakaba.mp3", "Mamady Keita - Donaba, Nfakaba.mp3", "Moussa konde - Nfakaba.mp3"], pcfiles : ["N'fa Kaba.pcc"], notes: ["<p>Here's a link to my 4 recordings of N'fakaba:</p><a href='https://www.dropbox.com/sh/mhq6yb6f9lcwzkn/aTpjcSqUYp'>https://www.dropbox.com/sh/mhq6yb6f9lcwzkn/aTpjcSqUYp</a><p>1) Diarra Konate - N'fakaba from start to finish.</p><p>2) Famoudou Konate - song/narration by a djelimoussa, then Konowulen II comes in at 1:04. N'fakaba starts at 2:55 with song starting at 3:55.</p><p>3) Mamady Keita - starts w Donaba. N'fakaba starts at 3:33.</p><p>4) Moussa Konde - this is my favorite for D var's, but the kenkeni is absent. Was recorded in the village of Kouroussa, Hamanah.</p><p>What I realized after listening to these is that, unlike what I originally thought and said--that the S&D players do a lot of improvising in the 2nd half of the rhythm (due to Cali boys playing it that way)--the S does very little improv, if any, in the 2nd half, in all 4 of these recordings, while the D does lots of improvising throughout the whole cycle.I wouldn't mind playing it either way though.Enjoy!K.</p>"] },
+
+"Senefoli/Soro": { audio : [], pcfiles : ["Senefoli.pcc"] },
+
+"Sofa": { audio : [], pcfiles : ["Sofa-basic.pcc"] },
+
+"Soli": { audio : [], pcfiles : ["Soli root and echauff.pcc"], videos: ['<iframe width="560" height="315" src="http://www.youtube.com/embed/wnCiNlunT3g?list=PLC5AAFA21E68EED43" frameborder="0" allowfullscreen></iframe>'] },
+
+"Sunu": { audio : [], pcfiles : ["Sunu_0.pcc"] },
+
+"Takosaba": { audio : [], pcfiles : ["Takosaba.pcc", "Takosaba2.pcc"] },
+
+"Toro": { audio : [], pcfiles : ["Toro.pcc"] },
+
+"Wurende" : { audio : ["Famoudou Konate - Wurende.mp3"], pcfiles : []}
+
+}
